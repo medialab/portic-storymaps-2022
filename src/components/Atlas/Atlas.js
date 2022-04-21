0bed1c4ddc3d904a80d69dbe560d1046b6b28144
Myllaume <guillaume.brioudes@myllaume.fr>
2022-04-21 17:46:27 +0200

    atlas focus (bug)

--- src/components/Atlas/Atlas.js
+++ src/components/Atlas/Atlas.js
@@ -3,50 +3,104 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { Helmet } from "react-helmet";
 import cx from 'classnames';
 
-// charger le json de la liste des visualisations de l'atlas
 import visualizationsMetas from '../../data/viz';
-import VisualizationFocus from '../../components/VisualizationFocus/VisualizationFocus';
+import Loader from '../Loader';
+import VisualizationFocus from '../VisualizationFocus';
 
 import translate from '../../utils/translate';
 import { buildPageTitle } from '../../utils/misc';
+import { fetchDataCsv } from '../../utils/fetch';
 
 import './Atlas.scss';
+
 
 export default function Atlas({
     ...props
 }) {
     const { vizId, lang } = useParams();
     const navigate = useNavigate();
-    console.log('vizId', vizId);
+
+    const [data, setData] = useState(undefined);
+    const [vizMetas, setVizMetas] = useState(undefined);
+    const [vizIdToFocus, setVizIdToFocus] = useState(vizId);
+    /** @type {['process'|'failed'|'successed'|'none', Function]} */
+    const [loadingState, setLoadingState] = useState('none');
 
     /**
      * Launch focus on a viz
-     * @param {Array} csvFiles 
+     * @param {String} vizId 
      */
-    function onClickFocus (vizId, csvFiles) {
-        navigate( `/${lang}/atlas/${vizId}`);
+    function onClickFocus (vizId) {
+        navigate(`/${lang}/atlas/${vizId}`);
     }
 
-    return (
-        <div className='Atlas secondary-page'>
-            <Helmet>
-                <title>{buildPageTitle('Atlas', lang)}</title>
-            </Helmet>
-            <div className="centered-contents">
-                <h1 className='title'>{translate('atlas', 'title', lang)}</h1>
-                <ul>
-                    {
-                        Object.values(visualizationsMetas).map((metas, i) => {
-                            const title = metas['titre_' + lang] || false
-                                , description = metas['description_' + lang] || false;
+    useEffect(() => {
+        if (!!vizIdToFocus === false) {
+            setLoadingState('none');
+            return;
+        }
 
-                            const { id, output } = metas;
+        setLoadingState('process');
+        const { outputs, ...metas } = visualizationsMetas[vizIdToFocus];
 
-                            return <li onClick={() => onClickFocus(id, output)}>{title}</li>
-                        })
-                    }
-                </ul>
+        Promise.all(
+            outputs.map(fileToLoad => fetchDataCsv(fileToLoad))
+        )
+        .then((datasets) => {
+            let payload = {};
+            for (let i = 0; i < datasets.length; i++) {
+                payload[outputs[i]] = datasets[i];
+            }
+            setData(payload);
+            setVizMetas(metas);
+            setLoadingState('successed');
+        })
+        .catch((error) => {
+            setLoadingState('failed');
+            console.error(error);
+        })
+
+    }, [vizIdToFocus])
+
+    if (loadingState === 'none') {
+        return (
+            <div className='Atlas secondary-page'>
+                <Helmet>
+                    <title>{buildPageTitle('Atlas', lang)}</title>
+                </Helmet>
+                <div className="centered-contents">
+                    <h1 className='title'>{translate('atlas', 'title', lang)}</h1>
+                    <ul>
+                        {
+                            Object.values(visualizationsMetas).map((metas, i) => {
+                                const title = metas['titre_' + lang] || false
+                                    , description = metas['description_' + lang] || false;
+    
+                                const { id, output } = metas;
+    
+                                return <li onClick={() => onClickFocus(id, output)}>{title}</li>
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    if (loadingState === 'process') {
+        return <Loader message='En cours de chargement' />
+    }
+    if (loadingState === 'failed') {
+        return <Loader message='Échec du chargement' />
+    }
+
+    if (loadingState === 'successed') {
+        return (
+            <VisualizationFocus
+                focusedVizId={vizIdToFocus}
+                data={data}
+                onClickClose={(e) => navigate(`/${lang}/atlas`)}
+            />
+        )
+    }
 }
