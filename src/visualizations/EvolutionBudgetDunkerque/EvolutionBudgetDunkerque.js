f124de67c3d3c695fe18606b0db84be37bcae016
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2022-06-08 14:51:03 +0200

    linechart budget dunkerque

--- src/visualizations/EvolutionBudgetDunkerque/EvolutionBudgetDunkerque.js
+++ src/visualizations/EvolutionBudgetDunkerque/EvolutionBudgetDunkerque.js
@@ -3,23 +3,11 @@ import React, { useMemo } from 'react';
 import LineChart from '../../components/LineChart';
 
 export default function EvolutionBudgetDunkerque({
-    data: inputData,
+    data,
     dimensions,
     ...props
 }) {
     const { width, height } = dimensions;
-
-    const data = useMemo(function prepareData() {
-        let preparedData = inputData.map((row) => {
-            return {
-                ['Années comptables']: +row['Années comptables'],
-                ['Recettes']: +row['Recettes'],
-                ['Dépenses']: +row['Dépenses'],
-                ['Soldes comptables']: +row['Soldes comptables']
-            }
-        })
-        return preparedData;
-    }, [inputData]);
 
     return (
         <LineChart
@@ -32,7 +20,10 @@ export default function EvolutionBudgetDunkerque({
                 field: 'Années comptables'
             }}
             y={{
-                field: 'Recettes'
+                field: 'valeur'
+            }}
+            color={{
+                field: 'référence'
             }}
         />
     )
