0eb101abec8109b8b7cc043eee31280fd64c197e
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2022-06-23 15:30:28 +0200

    update dunkerque map

--- src/visualizations/HistoireDunkerque/HistoireDunkerque.js
+++ src/visualizations/HistoireDunkerque/HistoireDunkerque.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React, { useMemo, useState } from "react";
+import iwanthue from "iwanthue";
 
-import { VisualisationContext } from "../../utils/contexts";
-
+import DunkerqueMap from "./DunkerqueMap";
 import Timeline from "./Timeline";
+import MapPoints from "./MapPoints";
 
-import './MapDunkerquePort.scss'
+import './HistoireDunkerque.scss';
 
 /**
  * 
@@ -25,73 +26,166 @@ export default function HistoireDunkerque({
     callerProps,
     ...props
 }) {
-    const [mapBackgroundPath, setMapBackgroundPath] = useState(undefined);
-    const [mapLayerPath, setMapLayerPath] = useState(undefined);
-    const [yearMark, setYearMark] = useState(undefined);
+    const { width, height } = dimensions;
+    const imgBasePath = `${process.env.BASE_PATH}/assets/`;
+    const timelineHeight = 50;
+    const asideWidth = 50;
 
-    const data = useMemo(() => {
-        return inputData.map(({...rest}) => {
-            const {year, object} = rest;
-            return {
-                id: [year, object].join('_'),
-                ...rest
+    const palette = {
+        'tax-free': 'url(#diag-hatch)',
+        'Spain': '#f5f697',
+        'France': '#2F2D8D',
+        'UK': '#a187d1'
+    }
+
+    const [diplayedYear, setDiplayedYear] = useState(1795);
+
+    const data = useMemo(function computeDataYears() {
+        return inputData.get('histoire-dunkerque-dates.csv').map((row, i) => {
+            const { year_start, year_end } = row;
+
+            if (year_start && year_end) {
+                return {
+                    ...row,
+                    type: 'period',
+                    year_start: +year_start,
+                    year_end: +year_end
+                }
             }
-        });
+            if (year_start) {
+                return {
+                    ...row,
+                    type: 'event',
+                    year: +year_start,
+                }
+            }
+        })
     }, [inputData]);
 
-    const mapDimensions = useMemo(() => {
-        return {
-            width: dimensions.width,
-            height: dimensions.height * 0.7
+    const events = data.filter(({ type }) => type === 'event').sort(({ year: aYear }, { year: bYear }) => {
+        if (aYear < bYear) { return -1; }
+        if (aYear > bYear) { return 1; }
+        return 0;
+    });
+    const periods = data.filter(({ type }) => type === 'period');
+
+    const {
+        dunkerqueLayer,
+        file_legend,
+        file_head
+    } = useMemo(function getDispleyForCurrentYear() {
+        let lastEvent = events.find(({ year }) => year === diplayedYear);
+        if (lastEvent === undefined) {
+            for (let i = 0; i < events.length; i++) {
+                const event = events[i];
+                if (diplayedYear < event['year']) { break; }
+                lastEvent = event;
+            }
         }
-    }, [dimensions]);
-
-    const timelineDimension = useMemo(() => {
-        return {
-            width: dimensions.width,
-            height: dimensions.height * 0.3
+        if (lastEvent === undefined) {
+            lastEvent = events[0];
         }
-    }, [dimensions]);
+        const { file_legend, file_head } = lastEvent;
+        for (const period of periods) {
+            const {
+                year_start,
+                year_end,
+                dunkerque_city,
+                dunkerque_territory,
+                country,
+                upper_town,
+                lower_town
+            } = period;
+            if (year_start <= diplayedYear && year_end > diplayedYear) {
+                return {
+                    dunkerqueLayer: {
+                        upperTown: dunkerque_city === '1' && upper_town === 'tax-free' ? 'red' : 'transparent',
+                        lowerTown: dunkerque_city === '1' && lower_town === 'tax-free' ? 'red' : 'transparent',
+                        dunkerqueTerritory: dunkerque_territory === '1' ? palette[upper_town] : 'transparent',
+                        country: country === '1' ? palette['France'] : 'transparent',
+                    },
+                    file_legend,
+                    file_head
+                }
+            }
+        }
+        return {};
+    }, [diplayedYear, data])
 
-    useEffect(() => {
-        const path = process.env.BASE_PATH + '/assets';
-        setMapBackgroundPath(path + '/dunkerque_map.jpg');
-    }, []);
-
-    const mapRef = useRef(null);
-
-    function changeMapView(id, xCoordinate) {
-        const { file, year } = data.find(({id: rowId}) => rowId === id);
-        const pathSvg = process.env.BASE_PATH + '/assets';
-        setMapLayerPath(pathSvg + '/' + file);
-        setYearMark({
-            year,
-            x: xCoordinate
-        });
-    }
-
-    function resetMapView() {
-        setMapLayerPath(undefined);
-        setYearMark(undefined);
-    }
-    
     return (
-        <div ref={mapRef} className='MapDunkerquePort' style={{ ...dimensions }}>
-            <div className="map-container" style={{ ...mapDimensions }}>
-                <img className='layer' src={mapLayerPath} />
-                <img className='base' src={mapBackgroundPath} />
+        <div
+            className='HistoireDunkerque'
+            style={{
+                width,
+                height
+            }}
+        >
+            <div
+                className="top-section"
+                style={{
+                    width,
+                    height: height - timelineHeight
+                }}
+            >
+                <div
+                    className='map-aside'
+                    style={{
+                        height: height - timelineHeight
+                    }}
+                >
+                    <img
+                        src={imgBasePath + file_head}
+                    />
+                    <img
+                        src={imgBasePath + file_legend}
+                    />
+                </div>
+                <div
+                    className="map-main map-container"
+                    style={{
+                        width: width - asideWidth,
+                        height: height - timelineHeight
+                    }}
+                >
+                    <div
+                        className='layer'
+                        style={{ zIndex: 200 }}
+                    >
+                        <MapPoints
+                            height={height - timelineHeight}
+                            data={inputData.get('histoire-dunkerque-points.csv')}
+                            diplayedYear={diplayedYear}
+                        />
+                    </div>
+                    <div
+                        className='layer'
+                        style={{ zIndex: 100 }}
+                    >
+                        <DunkerqueMap
+                            dimensions={{
+                                width: width - asideWidth,
+                                height: height - timelineHeight
+                            }}
+                            elementsDisplay={dunkerqueLayer}
+                        />
+                    </div>
+                    <img
+                        className='base'
+                        src={imgBasePath + 'dunkerque_map.jpg'}
+                    />
+                </div>
             </div>
-
-            <VisualisationContext.Provider value={{
-                changeMapView,
-                resetMapView,
-                yearMark
-            }}>
+            <footer className="timeline">
                 <Timeline
-                    { ...{ data, callerProps } }
-                    dimensions={timelineDimension}
+                    dimensions={{
+                        width,
+                        height: timelineHeight
+                    }}
+                    palette={palette}
+                    data={data}
+                    yearState={[diplayedYear, setDiplayedYear]}
                 />
-            </VisualisationContext.Provider>
+            </footer>
         </div>
     )
 }
