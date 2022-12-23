6356baffd5b9bf8860466c31273a405871fbccd7
Robin de Mourat <robin.demourat@gmail.com>
2022-12-23 11:20:03 +0100

    improve valeur-par-tonneau #102

--- src/visualizations/ValeurParTonneau/ValeurParTonneau.js
+++ src/visualizations/ValeurParTonneau/ValeurParTonneau.js
@@ -1,6 +1,8 @@
 import { useState, useMemo } from "react";
 import BarChart from "../../components/BarChart";
 import translate from '../../utils/translate';
+import { formatNumber } from "../../utils/misc";
+import './ValeurParTonneau.scss';
 
 const ValeurParTonneau = ({
   data,
@@ -17,16 +19,73 @@ const ValeurParTonneau = ({
       }
       return -1;
     })
-    // .filter(a => +a['price_per_barrel'] > 0)
-    .map(a => ({...a, [field]: +a[field]}))
+      // .filter(a => +a['price_per_barrel'] > 0)
+      .map(a => ({ ...a, [field]: +a[field] }))
   }
-  , [data, field])
+    , [data, field])
   return (
-    <>
-    <BarChart
+    <div className="ValeurParTonneau">
+      <div className="columns-container">
+        <div className="column">
+          <h2>Exports selon toflit18</h2>
+          <BarChart
             {...{
               data: actualData,
-              width,
+              width: width / 3,
+              height,
+            }}
+
+            layout='stack'
+            orientation='vertical'
+            x={{
+              field: 'sum_exports',
+              domain: [0, 110000000],
+              tickSpan: 50000000,
+              tickFormat: d => `${formatNumber(d)} lt`,
+              title: 'somme des exports', // translate('TonnagesF12', 'with_lest_title', lang)
+            }}
+            y={{
+              field: 'partner',
+              title: 'partenaire', //  translate('TonnagesF12', 'destination', lang)
+            }}
+            tooltip={
+              (d) => `${formatNumber(parseInt(+d.sum_exports))} lt exportés vers le partenaire ${d.partner}`
+            }
+          />
+        </div>
+        <div className="column">
+          <h2>Tonnages selon F12</h2>
+          <BarChart
+            {...{
+              data: actualData,
+              width: width / 3,
+              height,
+            }}
+
+            layout='stack'
+            orientation='vertical'
+            x={{
+              field: withLest ? 'sum_tonnage' : 'sum_tonnage_without_lest',
+              domain: [0, 400000],
+              tickSpan: 100000,
+              tickFormat: d => `${formatNumber(d)} tx`,
+              title: 'somme des tonnages', // translate('TonnagesF12', 'with_lest_title', lang)
+            }}
+            y={{
+              field: 'partner',
+              title: 'partenaire', //  translate('TonnagesF12', 'destination', lang)
+            }}
+            tooltip={
+              (d) => `${formatNumber(d[withLest ? 'sum_tonnage' : 'sum_tonnage_without_lest'])} tonneaux cumulés envoyés vers le partenaire ${d.partner}`
+            }
+          />
+        </div>
+        <div className="column">
+           <h2>Prix par tonneau</h2>
+          <BarChart
+            {...{
+              data: actualData,
+              width: width / 3,
               height,
             }}
 
@@ -34,7 +93,8 @@ const ValeurParTonneau = ({
             orientation='vertical'
             x={{
               field: field,
-              domain: [0, 1200],
+              domain: [0, 1000],
+              tickSpan: 200,
               tickFormat: d => `${d} lt./t`,
               title: 'prix par tonneau', // translate('TonnagesF12', 'with_lest_title', lang)
             }}
@@ -43,23 +103,25 @@ const ValeurParTonneau = ({
               title: 'partenaire', //  translate('TonnagesF12', 'destination', lang)
             }}
             // color={{
-              // field: 'tonnage_hypothese_avec_lest',
-              // title:  // translate('PecheTypeValue', 'color', lang)
+            // field: 'tonnage_hypothese_avec_lest',
+            // title:  // translate('PecheTypeValue', 'color', lang)
             // }}
 
             tooltip={
               (d) => `${d[field].toFixed(2)} lt par tonneau pour le partenaire ${d.partner}`
             }
           />
-      <div className="buttons-container" style={{margin: '1rem'}}>
-        <button className={`Button ${withLest ? 'is-active': ''}`} onClick={() => setWithLest(true)}>
+        </div>
+      </div>
+      <div className="buttons-container" style={{ margin: '1rem' }}>
+        <button className={`Button ${withLest ? 'is-active' : ''}`} onClick={() => setWithLest(true)}>
           {translate('TonnagesF12', 'hyp_with_lest', lang)}
         </button>
-        <button className={`Button ${!withLest ? 'is-active': ''}`} onClick={() => setWithLest(false)}>
+        <button className={`Button ${!withLest ? 'is-active' : ''}`} onClick={() => setWithLest(false)}>
           {translate('TonnagesF12', 'hyp_without_lest', lang)}
         </button>
       </div>
-    </>
+    </div>
   )
 }
 
