1c4e9f827ecbfa2eb7bd4c3c76a87b8045335fb0
Robin de Mourat <robin.demourat@gmail.com>
2022-12-23 11:19:40 +0100

    improve tonnages-1787-f12 #100

--- src/visualizations/TonnageF12/TonnageF12.js
+++ src/visualizations/TonnageF12/TonnageF12.js
@@ -2,6 +2,7 @@ import { useState, useMemo } from "react";
 import BarChart from "../../components/BarChart";
 import { formatNumber } from "../../utils/misc";
 import translate from '../../utils/translate';
+import './TonnageF12.scss';
 
 const TonnageF12 = ({
   data,
@@ -12,56 +13,99 @@ const TonnageF12 = ({
   const [withLest, setWithLest] = useState(true);
   const field = withLest ? 'tonnage_hypothese_avec_lest' : 'tonnage_hypothese_sans_lest';
   const actualData = useMemo(() => {
-    return data.sort((a, b) => {
+    const original = data.get('tonnages_f12_1787.csv')
+    const aggregated = data.get('tonnages_f12_1787_aggregated_by_grouping.csv')
+    const sorted = [original, aggregated].map(dataset => dataset.sort((a, b) => {
       if (a['destination'] > b['destination']) {
         return 1;
       }
       return -1;
     })
-    // .filter(a => +a['tonnage_hypothese_avec_lest'] > 0)
-    .map(a => ({...a, [field]: +a[field]}))
+      // .filter(a => +a['tonnage_hypothese_avec_lest'] > 0)
+      .map(a => ({ ...a, [field]: +a[field] }))
+    );
+    return {
+      original: sorted[0],
+      aggregated: sorted[1]
+    }
   }
-  , [data, field])
+    , [data, field])
   return (
-    <>
-    <BarChart
+    <div className="TonnageF12">
+      <div className="columns-container">
+        <div className="column">
+          <h2>Partenaires de la source F12</h2>
+          <BarChart
             {...{
-              data: actualData,
-              width,
-              height,
+              data: actualData.original,
+              width: width / 2,
+              height: height - 50,
             }}
 
             layout='stack'
             orientation='vertical'
             x={{
               field: field,
-              domain: [0, 90000],
+              domain: [0, 120000],
               tickFormat: d => `${formatNumber(d)} tx`,
-              title:  translate('TonnagesF12', 'with_lest_title', lang)
+              tickSpan: 20000,
+              title: translate('TonnagesF12', 'with_lest_title', lang)
             }}
             y={{
               field: 'destination',
               title: translate('TonnagesF12', 'destination', lang)
             }}
-            // color={{
-              // field: 'tonnage_hypothese_avec_lest',
-              // title:  // translate('PecheTypeValue', 'color', lang)
-            // }}
 
             tooltip={
               // @todo translate
               (d) => `${formatNumber(d[field])} tonneaux envoyé au partenaire ${d.destination} en 1787`
             }
           />
-      <div className="buttons-container" style={{margin: '1rem'}}>
-        <button className={`Button ${withLest ? 'is-active': ''}`} onClick={() => setWithLest(true)}>
+        </div>
+        <div className="column centered-arrow-container">
+          <div className="centered-arrow">
+          →
+          </div>
+        </div>
+        <div className="column">
+          <h2>Partenaires F12 alignés sur toflit18 "grouping"</h2>
+          <BarChart
+            {...{
+              data: actualData.aggregated,
+              width: width / 2,
+              height: height - 50,
+            }}
+
+            layout='stack'
+            orientation='vertical'
+            x={{
+              field: field,
+              domain: [0, 270000],
+              tickFormat: d => `${formatNumber(d)} tx`,
+              tickSpan: 50000,
+              title: translate('TonnagesF12', 'with_lest_title', lang)
+            }}
+            y={{
+              field: 'destination',
+              title: translate('TonnagesF12', 'destination', lang)
+            }}
+
+            tooltip={
+              // @todo translate
+              (d) => `${formatNumber(d[field])} tonneaux envoyé au partenaire ${d.destination} en 1787`
+            }
+          />
+        </div>
+      </div>
+      <div className="buttons-container" style={{ margin: '1rem' }}>
+        <button className={`Button ${withLest ? 'is-active' : ''}`} onClick={() => setWithLest(true)}>
           {translate('TonnagesF12', 'hyp_with_lest', lang)}
         </button>
-        <button className={`Button ${!withLest ? 'is-active': ''}`} onClick={() => setWithLest(false)}>
+        <button className={`Button ${!withLest ? 'is-active' : ''}`} onClick={() => setWithLest(false)}>
           {translate('TonnagesF12', 'hyp_without_lest', lang)}
         </button>
       </div>
-    </>
+    </div>
   )
 }
 
