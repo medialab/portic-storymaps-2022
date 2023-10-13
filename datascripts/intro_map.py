7da9555e94e720dc355cf86ed418fef801cf9291
Robin de Mourat <robin.demourat@gmail.com>
2023-10-13 16:45:33 +0200

    fix issue with maps copying

--- datascripts/intro_map.py
+++ datascripts/intro_map.py
@@ -17,11 +17,11 @@ if not os.path.exists(output_folder_path):
    os.makedirs(output_folder_path)
 shutil.copyfile(input_path, output_path)
 
-input_path = "./resources/physical_world_map_light.geojson"
-output_path = "../public/data/map_backgrounds/physical_world_map_light.geojson"
-output_folder_path = "../public/data/map_backgrounds"
-print('copying physical world map light')
-if not os.path.exists(output_folder_path):
-   os.makedirs(output_folder_path)
-shutil.copyfile(input_path, output_path)
+# input_path = "./resources/physical_world_map_light.geojson"
+# output_path = "../public/data/map_backgrounds/physical_world_map_light.geojson"
+# output_folder_path = "../public/data/map_backgrounds"
+# print('copying physical world map light')
+# if not os.path.exists(output_folder_path):
+#    os.makedirs(output_folder_path)
+# shutil.copyfile(input_path, output_path)
 
