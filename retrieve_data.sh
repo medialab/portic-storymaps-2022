47e866023cffc2c7861b2a9a08eb88995fbcd8ac
Robin de Mourat <robin.demourat@gmail.com>
2024-10-22 16:26:01 +0200

    isolate data sources close #157

--- retrieve_data.sh
+++ retrieve_data.sh
@@ -1,14 +1,25 @@
-echo "Download data : fetching latest toflit18 data"
-curl -o data/toflit18_bdd.zip "https://raw.githubusercontent.com/medialab/toflit18_data/master/base/bdd%20courante.csv.zip"
-unzip data/toflit18_bdd.zip -d "data"
-rm -f data/toflit18_bdd.zip
-mv "data/bdd courante.csv" "data/toflit18_all_flows.csv"
-echo "Download data : fetching latest navigo pointcalls data"
-curl -o data/navigo_all_pointcalls_1789.csv "data.portic.fr/api/pointcalls/?date=1789&format=csv"
-curl -o data/navigo_all_pointcalls_1787.csv "data.portic.fr/api/pointcalls/?date=1787&format=csv"
-echo "Download data : fetching latest navigo flows data"
-curl -o data/navigo_all_flows_1789.csv "data.portic.fr/api/rawflows/?date=1789&format=csv"
-curl -o data/navigo_all_flows_1787.csv "data.portic.fr/api/rawflows/?date=1787&format=csv"
+echo "Download data: fetching isolated toflit18 data"
+curl -o "data/toflit18_all_flows.csv" "https://sandbox.robindemourat.com/portic-storymaps-data/toflit18_all_flows.csv"
+
+echo "Download data: fetching isolated navigo pointcalls data"
+curl -o data/navigo_all_pointcalls_1789.csv "https://sandbox.robindemourat.com/portic-storymaps-data/navigo_all_pointcalls_1789.csv"
+curl -o data/navigo_all_pointcalls_1787.csv "https://sandbox.robindemourat.com/portic-storymaps-data/navigo_all_pointcalls_1787.csv"
+
+echo "Download data: fetching isolated navigo flows data"
+curl -o data/navigo_all_flows_1789.csv "https://sandbox.robindemourat.com/portic-storymaps-data/navigo_all_flows_1789.csv"
+curl -o data/navigo_all_flows_1787.csv "https://sandbox.robindemourat.com/portic-storymaps-data/navigo_all_flows_1787.csv"
+
+# echo "Download data : fetching latest toflit18 data"
+# curl -o data/toflit18_bdd.zip "https://raw.githubusercontent.com/medialab/toflit18_data/master/base/bdd%20courante.csv.zip"
+# unzip data/toflit18_bdd.zip -d "data"
+# rm -f data/toflit18_bdd.zip
+# mv "data/bdd courante.csv" "data/toflit18_all_flows.csv"
+# echo "Download data : fetching latest navigo pointcalls data"
+# curl -o data/navigo_all_pointcalls_1789.csv "data.portic.fr/api/pointcalls/?date=1789&format=csv"
+# curl -o data/navigo_all_pointcalls_1787.csv "data.portic.fr/api/pointcalls/?date=1787&format=csv"
+# echo "Download data : fetching latest navigo flows data"
+# curl -o data/navigo_all_flows_1789.csv "data.portic.fr/api/rawflows/?date=1789&format=csv"
+# curl -o data/navigo_all_flows_1787.csv "data.portic.fr/api/rawflows/?date=1787&format=csv"
 
 cd datascripts
 for f in *.py; do echo "execute python script $f"; python3 "$f"; done
